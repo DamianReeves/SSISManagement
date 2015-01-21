9711b533648bcc1601939e570ca7405b5fc44923
Damian Reeves <damreev@gmail.com>
2015-01-21 08:36:29 -0500

    Created F# project

---
+++ src/Eleven19.Ssis.Management/ConnectionStrings.fs
@@ -0,0 +1,9 @@
+﻿namespace Eleven19.Ssis.Management
+
+[<AutoOpen>]
+module ConnectionStrings =
+    type ConnectionStringOrName =
+    | ConnectionString of string
+    | ConnectionName of string
+
+
---
+++ src/Eleven19.Ssis.Management/Script.fsx
@@ -0,0 +1,8 @@
+﻿// Learn more about F# at http://fsharp.net. See the 'F# Tutorial' project
+// for more guidance on F# programming.
+
+#load "ConnectionStrings.fs"
+open Eleven19.Ssis.Management
+
+// Define your library scripting code here
+
